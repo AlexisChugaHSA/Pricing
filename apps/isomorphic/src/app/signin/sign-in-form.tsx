bfbe9c71ccf5ccf9c2d249d1c45f2b1662fcac48
AlexisChugaHSA <alexis.chuga@hsa.com.ec>
2024-11-13 11:56:34 -0500

    first commit

---
+++ apps/isomorphic/src/app/signin/sign-in-form.tsx
@@ -0,0 +1,92 @@
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
+import { signIn } from 'next-auth/react';
+import { SubmitHandler } from 'react-hook-form';
+import { PiArrowRightBold } from 'react-icons/pi';
+import { Checkbox, Password, Button, Input, Text } from 'rizzui';
+import { Form } from '@ui/form';
+import { routes } from '@/config/routes';
+import { loginSchema, LoginSchema } from '@/validators/login.schema';
+
+const initialValues: LoginSchema = {
+  email: 'admin@admin.com',
+  password: 'admin',
+  rememberMe: true,
+};
+
+export default function SignInForm() {
+  //TODO: why we need to reset it here
+  const [reset, setReset] = useState({});
+
+  const onSubmit: SubmitHandler<LoginSchema> = (data) => {
+    console.log(data);
+    signIn('credentials', {
+      ...data,
+    });
+  };
+
+  return (
+    <>
+      <Form<LoginSchema>
+        validationSchema={loginSchema}
+        resetValues={reset}
+        onSubmit={onSubmit}
+        useFormProps={{
+          defaultValues: initialValues,
+        }}
+      >
+        {({ register, formState: { errors } }) => (
+          <div className="space-y-5">
+            <Input
+              type="email"
+              size="lg"
+              label="Correo"
+              placeholder="Ingrese su correo"
+              className="[&>label>span]:font-medium"
+              inputClassName="text-sm"
+              {...register('email')}
+              error={errors.email?.message}
+            />
+            <Password
+              label="Contraseña"
+              placeholder="Ingrese su contraseña"
+              size="lg"
+              className="[&>label>span]:font-medium"
+              inputClassName="text-sm"
+              {...register('password')}
+              error={errors.password?.message}
+            />
+            <div className="flex items-center justify-between pb-2">
+              <Checkbox
+                {...register('rememberMe')}
+                label="Remember Me"
+                className="[&>label>span]:font-medium"
+              />
+              <Link
+                href={routes.auth.forgotPassword1}
+                className="h-auto p-0 text-sm font-semibold text-blue underline transition-colors hover:text-gray-900 hover:no-underline"
+              >
+                ¿Olvidaste tu contraseña?
+              </Link>
+            </div>
+            <Button className="w-full" type="submit" size="lg">
+              <span>Iniciar Sesión</span>{' '}
+              <PiArrowRightBold className="ms-2 mt-0.5 h-5 w-5" />
+            </Button>
+          </div>
+        )}
+      </Form>
+      <Text className="mt-6 text-center leading-loose text-gray-500 lg:mt-8 lg:text-start">
+      ¿No tienes una cuenta? {' '}
+        <Link
+          href={routes.auth.signUp1}
+          className="font-semibold text-gray-700 transition-colors hover:text-blue"
+        >
+          Registrate
+        </Link>
+      </Text>
+    </>
+  );
+}
